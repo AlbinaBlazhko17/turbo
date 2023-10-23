fd6158981d84b8817aea8c2ea3f4e16bf8266e82
Albinator <albinamivina@gmail.com>
2023-10-23 16:50:32 +0300

    feat: add aside component

---
+++ src/components/Aside/Aside.jsx
@@ -0,0 +1,37 @@
+import PropTypes from 'prop-types';
+
+import './aside.scss';
+
+function Aside ({title, data}) {
+	return (
+		<aside className="aside">
+			<h2>{title}</h2>
+			<div className="aside__wrapper">
+				{
+					data.length && data.map(el => (
+						<article key={el.key} className="aside-item">
+							<div><img src={el.img} alt="aside_image"/></div>
+							<div>
+								<h3 className="aside-item__header">{el.header}</h3>
+								<p className="aside-item__text">{el.text}</p>
+							</div>
+						</article>
+					))
+				}
+			</div>
+		</aside>
+	)
+}
+
+Aside.propTypes = {
+	title: PropTypes.string.isRequired,
+	data: PropTypes.arrayOf(
+		PropTypes.shape({
+			key: PropTypes.string.isRequired,
+			header: PropTypes.string.isRequired,
+			text: PropTypes.string.isRequired,
+		})
+	).isRequired,
+  };
+
+export default Aside;
--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,12 +1,53 @@
 import Stats from '../Stats/Stats';
+import Aside from '../Aside/Aside';
+import AsideImg from '@assets/img/aside_image.jpeg';
 
 import './dashboard.scss';
 
 function Dashboard () {
+	const dataForNews = [
+		{
+			img: AsideImg,
+			header: 'Financial updates',
+			text: 'It is a long established fact that a reader will be distracted by...'
+		},
+		{
+			img: AsideImg,
+			header: 'Financial updates',
+			text: 'It is a long established fact that a reader will be distracted by...'
+		},
+		{
+			img: AsideImg,
+			header: 'Financial updates',
+			text: 'It is a long established fact that a reader will be distracted by...'
+		},
+		{
+			img: AsideImg,
+			header: 'Financial updates',
+			text: 'It is a long established fact that a reader will be distracted by...'
+		},
+		{
+			img: AsideImg,
+			header: 'Financial updates',
+			text: 'It is a long established fact that a reader will be distracted by...'
+		},
+		{
+			img: AsideImg,
+			header: 'Financial updates',
+			text: 'It is a long established fact that a reader will be distracted by...'
+		},
+		{
+			img: AsideImg,
+			header: 'Financial updates',
+			text: 'It is a long established fact that a reader will be distracted by...'
+		}
+	];
+
 	return (
 		<main>
 			<h1>Dashboard</h1>
 			<Stats />
+			<Aside title='Top news' data={dataForNews} />
 		</main>
 	)
 }
