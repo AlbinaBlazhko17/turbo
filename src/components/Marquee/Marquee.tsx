5d36710e73fac0aed15c97c14f7c0baebd7a253a
Albinator <albinamivina@gmail.com>
2023-10-26 12:26:40 +0300

    reafactor: marquee animation

--- src/components/Marquee/Marquee.tsx
+++ src/components/Marquee/Marquee.tsx
@@ -1,26 +1,19 @@
+import dataForMarquee from '@utils/dataForMarquee';
 import style from './marquee.module.scss';
 
 function Marquee () {
+	const duplicatedItems: Array<string> = dataForMarquee.concat(dataForMarquee);
+
 	return (
 		<section className={style.marquee}>
-			<div className={style.marquee__wrapper}>
-				<div className={style.marquee__content}>
-					<h3 className={style['marquee-item']}>Turbo platform</h3>
-					<h3 className={style['marquee-item']}>Best experts</h3>
-					<h3 className={style['marquee-item']}>High-level services</h3>
-					<h3 className={style['marquee-item']}>Modern tools</h3>
-					<h3 className={style['marquee-item']}>Support & cooperation</h3>
-					<h3 className={style['marquee-item']}>Professional Team</h3>
-				</div>
-
-				<div className={style['marquee__content']}>
-					<h3 className={style['marquee-item']}>Turbo platform</h3>
-					<h3 className={style['marquee-item']}>Best experts</h3>
-					<h3 className={style['marquee-item']}>High-level services</h3>
-					<h3 className={style['marquee-item']}>Modern tools</h3>
-					<h3 className={style['marquee-item']}>Support & cooperation</h3>
-					<h3 className={style['marquee-item']}>Professional Team</h3>
-				</div>
+			<div className={style.marquee__mask}>
+				<ul className={style.marquee__wrapper}>
+					{duplicatedItems.map((item, index) => (
+						<li key={index} className={style['marquee-item']}>
+							{item}
+						</li>
+					))}
+				</ul>
 			</div>
 		</section>
 	)
---
+++ src/utils/dataForMarquee.ts
@@ -0,0 +1,10 @@
+const dataForMarquee: Array<string> = [
+	'Turbo platform',
+	'Best experts',
+	'High-level services',
+	'Modern tools',
+	'Support & cooperation',
+	'Professional Team',
+];
+
+export default dataForMarquee;
