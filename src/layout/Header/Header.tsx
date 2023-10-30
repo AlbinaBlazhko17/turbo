2e052829ecaefd5f59b5004775cf404cebddfb2f
Albinator <albinamivina@gmail.com>
2023-10-30 11:55:07 +0200

    feat: add context and switcher with custom styles

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -1,18 +1,42 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { ThemeContext } from '@pages/App/App';
 import ChatBlue from '@assets/icons/chat_blue.svg';
 import PersonPhoto from '@assets/img/person.png';
+import ReactSwitch from 'react-switch';
+import Sun from '@assets/icons/sun.svg';
+import Moon from '@assets/icons/moon.svg';
 
 import style from './header.module.scss';
 
 function Header () {
+	const {theme, toggleTheme} = useContext(ThemeContext);
+
 	return (
 		<header className={style.header}>
-			<Link to='/' className={style.header__img}>
-				<img src={ChatBlue} alt="chat"/>
-			</Link>
-			<Link to='/' className={style.header__img}>
-				<img src={PersonPhoto} alt="user"/>
-			</Link>
+			<div className={style.header__wrapper}>
+				<ReactSwitch
+					onChange={toggleTheme}
+					checked={theme === 'light'}
+					offColor='#5d7285'
+					onColor='#cacbcc'
+					onHandleColor='#2196f3'
+					offHandleColor='#2196f3'
+					uncheckedIcon={false}
+					checkedIcon={false}
+					uncheckedHandleIcon={<img src={Moon} alt="moon" style={{width: '30px', height: '30px', transform: 'rotate(40deg)'}}/>}
+					checkedHandleIcon={<img src={Sun} alt="sun" style={{width: '30px', height: '30px', transform: 'rotate(40deg)'}} />}
+					width={62}
+					height={32}
+					handleDiameter={30}
+				/>
+				<Link to='/' className={style.header__img}>
+					<img src={ChatBlue} alt="chat"/>
+				</Link>
+				<Link to='/' className={style.header__img}>
+					<img src={PersonPhoto} alt="user"/>
+				</Link>
+			</div>
 		</header>
 	)
 }
--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -1,18 +1,29 @@
+import { createContext, useState } from 'react';
 import { Routes, Route } from 'react-router';
 import routesConfig from '@routes/routes';
 import Layout from '@layout/Layout/Layout';
 
 import './App.css'
 
+export const ThemeContext = createContext({theme: 'light', toggleTheme: () => {}});
+
 function App() {
+	const [theme, setTheme] = useState('light');
+
+	const toggleTheme = () => {
+		setTheme(theme === 'light' ? 'dark' : 'light');
+	}
+
 	return (
-		<Routes>
-			<Route path='/' element={<Layout />}>
-				{routesConfig.map((route, i) => (
-					<Route key={i} {...route} />
-				))}
-			</Route>
-		</Routes>
+		<ThemeContext.Provider value={{theme, toggleTheme}}>
+			<Routes>
+				<Route path='/' element={<Layout />}>
+					{routesConfig.map((route, i) => (
+						<Route key={i} {...route} />
+					))}
+				</Route>
+			</Routes>
+		</ThemeContext.Provider>
 	)
 }
 
