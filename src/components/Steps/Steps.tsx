f7ba291bc42535a53bf08e6eee556a1474f34231
Albinator <albinamivina@gmail.com>
2023-10-30 18:15:33 +0200

    feat: add style to before

--- src/components/Steps/Steps.tsx
+++ src/components/Steps/Steps.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import dataForSteps from '@utils/dataForSteps';
 import { ThemeContext } from '@/theme/theme';
 import cn from 'classnames';
@@ -8,13 +8,14 @@ import style from './steps.module.scss';
 
 function Steps({currentStep, setCurrentStep}: {currentStep: number, setCurrentStep: () => void}) {
 	const { theme } = useContext(ThemeContext);
+	const [complete, setComplete] = useState(false);
 
 	return (
 		<section className={cn(style.steps, style[`${theme}`])}>
 			{
 				dataForSteps.map((step, i) => (
-					<div key={i} className={`${style['steps-item']} ${currentStep === i + 1 && style.active} ${ i + 1 < currentStep && style.complete}`}>
-						<h2 className={style['steps-item__step']}>{ i + 1 < currentStep? <TiTick size={24}/>: i + 1}</h2>
+					<div key={i} className={`${style['steps-item']} ${currentStep === i + 1 && style.active} ${ (i + 1 < currentStep || complete ) && style.complete}`}>
+						<h2 className={style['steps-item__step']}>{ (i + 1 < currentStep || complete)? <TiTick size={24}/>: i + 1}</h2>
 						<h3>{step}</h3>
 					</div>
 				))
