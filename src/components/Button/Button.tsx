4d4b8b1304b9d406adc3442dbaf3d747cedfa390
Albinator <albinamivina@gmail.com>
2023-10-31 16:10:28 +0200

    chore: remove imports

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,9 @@
 import cn from 'classnames';
 
 import style from './button.module.scss';
-import { useEffect } from 'react';
+
 
 function Button({children, appearance, className, ...props}: {children: string, appearance: 'outlined' | 'filled', className?: string}) {
-	useEffect(() =>{
-		console.log(className)
-	},[])
 	return(
 		<button className={cn(style.btn, className, {
 			[style['btn_outlined']]: appearance === 'outlined',
