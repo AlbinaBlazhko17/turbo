38f3ce0cc47d575ee561cb6ecc0bbe6b6823c441
Albinator <albinamivina@gmail.com>
2023-10-31 10:23:56 +0200

    feat: get and set steps in local storage

--- src/components/CustomForm/CustomForm.tsx
+++ src/components/CustomForm/CustomForm.tsx
@@ -1,8 +1,12 @@
 import style from './customForm.module.scss';
 
 function CustomForm () {
+	const currentStep = localStorage.getItem('step') || 1;
+
 	return (
 		<form className={style.form}>
+			{
+			}
 		</form>
 	)
 }
--- src/pages/FormPage/FormPage.tsx
+++ src/pages/FormPage/FormPage.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import CustomForm from '@components/CustomForm/CustomForm';
 import Steps from '@components/Steps/Steps';
 import Button from '@components/Button/Button';
@@ -6,17 +6,21 @@ import Button from '@components/Button/Button';
 import style from './formPage.module.scss';
 
 function FormPage () {
-	const [currentStep, setCurrentStep] = useState(1);
+	const [currentStep, setCurrentStep] = useState(localStorage.getItem('step') || 1);
+
+	useEffect(() => {
+		localStorage.setItem('step', currentStep.toString());
+	}, [currentStep]);
 
 	const handlePrevStep = () => {
-		if (currentStep > 1) {
-			setCurrentStep(currentStep - 1);
+		if (+currentStep > 1) {
+			setCurrentStep(+currentStep - 1);
 		}
 	}
 
 	const handleNextStep = () => {
-		if (currentStep < 4) {
-			setCurrentStep(currentStep + 1);
+		if (+currentStep < 4) {
+			setCurrentStep(+currentStep + 1);
 		}
 	}
 
@@ -24,7 +28,7 @@ function FormPage () {
 		<main>
 			<div>
 				<h1>Form page</h1>
-				<Steps currentStep={currentStep} setCurrentStep={setCurrentStep}/>
+				<Steps currentStep={+currentStep} setCurrentStep={setCurrentStep}/>
 				<CustomForm />
 				<Button appearance={currentStep === 1? 'outlined': 'filled'} onClick={handlePrevStep}>Previous step</Button>
 				<Button appearance={currentStep !== 4? 'filled': 'outlined' } onClick={handleNextStep}>{currentStep !== 4? 'Next step': 'Finish'}</Button>
