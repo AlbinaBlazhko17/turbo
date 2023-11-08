5bc3eb362fc7ecf7b3b08f04520849f4fb00b43f
Albinator <albinamivina@gmail.com>
2023-11-08 16:21:23 +0200

    fix: button prop types

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,7 +2,7 @@ import cn from 'classnames';
 
 import style from './button.module.scss';
 
-function Button({ children, appearance, className, onClick, type, ...props }: { children: string, appearance: 'outlined' | 'filled', className?: string, onClick: () => void, type?: 'button' | 'submit' }) {
+function Button({ children, appearance, className, onClick, type, ...props }: { children: string, appearance: 'outlined' | 'filled', className?: string, onClick?: () => void, type?: 'button' | 'submit' }) {
 	return (
 		<button className={cn(style.btn, className, {
 			[style['btn_outlined']]: appearance === 'outlined',
