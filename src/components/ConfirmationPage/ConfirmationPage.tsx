e5b3544e5d13d3f200bb4e8829f8e71626215f86
Albinator <albinamivina@gmail.com>
2023-11-09 17:06:16 +0200

    feat: add func to goToHome button in confirmation

--- src/components/ConfirmationPage/ConfirmationPage.tsx
+++ src/components/ConfirmationPage/ConfirmationPage.tsx
@@ -1,16 +1,27 @@
 import ConfirmIcon from '@assets/icons/confirm-icon.svg';
 import Button from '../Button/Button';
+import { useNavigate } from 'react-router';
+import { useDispatch } from 'react-redux';
+import { removeItemFromForm } from '@/store/actions';
 
 import style from './confirmationPage.module.scss';
 
 function ConfirmationPage() {
+	const navigator = useNavigate();
+	const formDisatcher = useDispatch();
+
+	function handleGoToHomePage() {
+		navigator('/');
+		localStorage.setItem('step', '1');
+		formDisatcher(removeItemFromForm);
+	}
 	return (
 		<div className={style.confirmation}>
 			<div className={style.confirmation__img}>
 				<img src={ConfirmIcon} alt="tick" />
 			</div>
 			<h3 className={style.confirmation__text}>All forms completed successfuly!</h3>
-			<Button appearance='filled' className={style.confirmation__button}>Go to home page</Button>
+			<Button appearance='filled' className={style.confirmation__button} onClick={handleGoToHomePage}>Go to home page</Button>
 		</div>
 	)
 }
--- src/components/CustomForm/initialValues.ts
+++ src/components/CustomForm/initialValues.ts
@@ -1,32 +1,8 @@
-import { IDataForPersonalnfoForm, IDataForAddressForm, IDataForPreferencesForm, IDataForSubmitForm } from "@/interfaces/IDataForForms"
+import { IDataForPersonalnfoForm } from "@/interfaces/IDataForForms"
 
 export const initialValuesPersonalInfo: IDataForPersonalnfoForm = {
 	firstName: '',
 	lastName: '',
 	email: '',
 	gender: '',
-}
-
-export const initialValuesAddress: IDataForAddressForm = {
-	city: '',
-	country: {
-		value: '',
-		label: '',
-	},
-	zipCode: null,
-}
-
-export const initialValuesPreferences: IDataForPreferencesForm = {
-	interests: [],
-	language: {
-		value: '',
-		label: '',
-	},
-	notificationFrequency: 0,
-}
-
-export const initialValuesSubmit: IDataForSubmitForm = {
-	comment: '',
-	profilePicture: null,
-	terms: false,
 }
