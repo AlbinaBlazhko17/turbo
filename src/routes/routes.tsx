aa6ae11c59559f3a8e23a9fbbff2006d57930320
Albinator <albinamivina@gmail.com>
2023-11-16 10:36:47 +0200

    feat: add errorhandler

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -3,12 +3,14 @@ import FormPage from '@pages/FormPage/FormPage';
 import Dashboard from '@pages/Dashboard/Dashboard';
 import Layout from '@/layout/Layout/Layout';
 import TablePage from '@/pages/TablePage/TablePage';
+import ErrorPage from '@/components/ErrorPage/ErrorPage';
 
 const routesConfig = createBrowserRouter(
 	[
 		{
 			path: "/",
 			element: <Layout />,
+			errorElement: <ErrorPage />,
 			children: [
 				{
 					path: "/",
